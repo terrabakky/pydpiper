c8e4fb44699f33679d2380d669678a6e50a3b270
Tyler Kieran Johnson <tyler.kieran.johnson@gmail.com>
2014-03-12 09:58:37 +1100

    Building up to some sort of queueing

--- scrape.py
+++ scrape.py
@@ -4,28 +4,37 @@
 import requests
 
 from lxml import html
-
+    
 class Scrape:
 
 	def __init__(self,target=None):
 		self.response_html = None
 		self.target = target
 		self.response = None
-
+		self.parsed_body = None
+		self.links = None
+		self.visited = set()
 		if target:
 			self.scrape(target)
+			self.parse_body()
 
 
 	def scrape(self, url):
+		if url in visited:
+			return False
+        
 		self.response = requests.get(url)
-		self.response
-		self.target = url
+		self.visited.add(url)
 		return True
+			
+	def parse_body(self):
+		self.parsed_body = html.fromstring(self.response.text)
+		self.links = self.parsed_body.xpath('//a/@href')
+        
+        
 
 
-	def links(self):
-		response_html().xpath('//a/@href')
-		return True
+		
 
 	# def clean_links(self):
 	# 	links = [link for link in links if link != '#']
@@ -34,14 +43,9 @@ class Scrape:
 x = Scrape('http://uow.edu.au')
 
 print x.response.content
-	
 
+print '##############'
 
-
-
-
-
-
-
+print x.links
 
 
