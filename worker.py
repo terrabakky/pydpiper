8a1082b292dafb54f2959429f4208ebfdb07d0fa
Tyler Kieran Johnson <tyler.kieran.johnson@gmail.com>
2014-03-17 14:53:16 +1100

    Changing back to methods from classes

--- worker.py
+++ worker.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
-
-from crawl import *
+import time
 from redis import Redis
 from rq import Queue
+from crawl import process
+from scrape import scrape
+import scrape
+
 
 # Tell RQ what Redis connection to use
 redis_conn = Redis()
 q = Queue(connection=redis_conn)
 
 START_LINK = "http://www.google.com"
-start = process(START_LINK)
+
+first_job = process(START_LINK)
+
+links = scrape.links
+
+visited = set()
+
+if first_job:
+	for link in links:
+		if not link in visited:
+			q.enqueue(process,link)
+			visited.add(link)
+
+	while True:
+		time.sleep(2)
+
+		print "Visited: " + str(visited)
+		# print "All Links: " + str(scrape.links)
 
 
-for link in start:
-	link = str(link)
-	if link.startswith('http://'):
-		q.enqueue(process,str(link))
+
+
